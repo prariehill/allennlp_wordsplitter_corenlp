8284629cf999e6baf9507299e1714fbd080c1d3f
liu xue yan <liu_xue_yan@foxmail.com>
2018-11-19 16:20:59 +0800

    add: remote and subproc CoreNLP WordSplitter class

---
+++ src/allennlp_wordsplitter_corenlp/wordspliters.py
@@ -0,0 +1,70 @@
+from typing import List
+
+from allennlp.common import Registrable
+from allennlp.data.tokenizers.token import Token
+from allennlp.data.tokenizers.word_splitter import WordSplitter
+from nltk.parse.corenlp import CoreNLPParser, CoreNLPServer
+from overrides import overrides
+
+
+@WordSplitter.register('corenlp_subproc')
+class CorenlpSubprocWordSplitter(WordSplitter):
+    """
+    A ``WordSplitter`` that uses CoreNLP's tokenizer.
+    It runs a ``corenlp-server`` as a sub-process, and call it's Web API.
+    """
+
+    def __init__(self,
+                 path_to_jar: str = None,
+                 path_to_models_jar: str = None,
+                 verbose: str = False,
+                 java_options: str = None,
+                 corenlp_options: str = None,
+                 port: int = None,
+                 encoding: str = 'utf8',
+                 ):
+        """
+        Parameters
+        ----------
+
+        * For parameters from ``path_to_jar`` to ``port``, see https://www.nltk.org/api/nltk.parse.html#nltk.parse.corenlp.
+        * For parameters ``encoding``,  see https://www.nltk.org/api/nltk.parse.html#nltk.parse.corenlp.CoreNLPParser
+        """
+        self._server = CoreNLPServer(
+            path_to_jar, path_to_models_jar, verbose, java_options, corenlp_options, port)
+        self._make_parser = lambda: CoreNLPParser(
+            self._server.url, encoding, 'pos')
+        self._server.start()
+
+    def __del__(self):
+        self._server.stop()
+
+    @overrides
+    def split_words(self, sentence: str) -> List[Token]:
+        parser = self._make_parser()
+        return [Token(t) for t in parser.tokenize(sentence)]
+
+
+@WordSplitter.register('corenlp_remote')
+class CorenlpRemoteWordSplitter(WordSplitter):
+    """
+    A ``WordSplitter`` that uses CoreNLP's tokenizer.
+    It calls ``corenlp-server``'s Web API.
+    """
+
+    def __init__(self,
+                 url: str = 'http://localhost:9000',
+                 encoding: str = 'utf8',
+                 ):
+        """
+        Parameters
+        ----------
+
+        see https://www.nltk.org/api/nltk.parse.html#nltk.parse.corenlp.CoreNLPParser
+        """
+        self._make_parser = lambda: CoreNLPParser(url, encoding, 'pos')
+
+    @overrides
+    def split_words(self, sentence: str) -> List[Token]:
+        parser = self._make_parser()
+        return [Token(t) for t in parser.tokenize(sentence)]
